5019f4104b848523415cdf62d65e2aa2cc041807
Sangbaek <sangbaek@mit.edu>
2020-12-04 16:57:41 -0500

    to test filtering compatilbe with clas12root

---
+++ sangbaek/run_filtering_full_test.groovy
@@ -0,0 +1,90 @@
+import org.jlab.jnp.hipo4.data.Event;
+import org.jlab.jnp.hipo4.data.Bank;
+import org.jlab.jnp.hipo4.io.HipoReader
+import org.jlab.jnp.hipo4.io.HipoWriter
+import org.jlab.jnp.hipo4.data.SchemaFactory
+import org.jlab.jnp.hipo4.operations.BankIterator;
+import org.jlab.jnp.hipo4.operations.BankSelector;
+import org.jlab.io.hipo.HipoDataEvent
+import org.jlab.io.hipo.HipoDataSource
+import org.jlab.groot.data.H1F
+import org.jlab.groot.data.H2F
+import org.jlab.groot.data.TDirectory
+import groovyx.gpars.GParsPool
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.Executors
+import java.util.concurrent.ScheduledExecutorService
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicInteger
+import sangbaek.filtering.filtering
+import event.Event
+import event.EventConverter
+import my.Sugar
+
+Sugar.enable()
+/////////////////////////////////////////////////////////////////////
+
+def outname = args[0].split('/')[-1]
+
+if (outname[-5..-1]!=".hipo"){
+  println("The input file name must end with \".hipo\".")
+  println("Halt.")
+  return
+}
+println("Filtering $outname..")
+outname = outname.substring(0,outname.lastIndexOf(".")) + "_filtered.hipo"
+println("Saving filtered file as $outname...\n\n\n")
+
+def processor = new filtering()
+
+def evcount = new AtomicInteger()
+def debug = {
+  println "event count: "+evcount.get()
+//  evcount.set(0)
+}
+
+def exe = Executors.newScheduledThreadPool(1)
+exe.scheduleWithFixedDelay(debug, 5, 30, TimeUnit.SECONDS)
+
+GParsPool.withPool 12, {
+  args.eachParallel{fname->
+
+    def reader = new HipoReader()
+    reader.open(fname)
+    SchemaFactory schema = reader.getSchemaFactory();
+   def writer = new HipoWriter(schema)
+    // SchemaFactory writerFactory = schema.reduce(["REC::Particle", "RUN::config", "REC::Event"]);
+    // def writer = new HipoWriter(writerFactory)
+
+    writer.open(outname)
+
+    BankIterator           iter = new BankIterator(4096);
+    BankSelector   dataSelector = new BankSelector(schema.getSchema("REC::Particle"));
+
+    def jnp_event = new org.jlab.jnp.hipo4.data.Event()
+
+    while(reader.hasNext() && evcount.get() < 5000) {
+      evcount.getAndIncrement()
+      reader.nextEvent(jnp_event)
+      if(!jnp_event.hasBank(reader.getSchemaFactory().getSchema("REC::Particle"))) continue
+      def data_event = new HipoDataEvent(jnp_event, reader.getSchemaFactory())
+      def event = EventConverter.convert(data_event)
+      def partlist = processor.filterEPGs(event)
+
+      if (partlist) {
+        dataSelector.getIterator(jnp_event, iter);
+        iter.reset()
+        partlist.each{iter.addIndex(it)}
+        Bank newBank = BankSelector.reduceBank(dataSelector.getBank(), iter);
+        jnp_event.remove(dataSelector.getBank().getSchema());
+        jnp_event.write(newBank);
+        writer.addEvent(jnp_event)
+      }
+    }
+    writer.close()
+    reader.close()
+  }
+}
+
+exe.shutdown()
+debug()
