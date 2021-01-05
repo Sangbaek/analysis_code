a3f593f1dfd0f1f90094ab8ca56baff0c3070349
Sangbaek <sangbaek@mit.edu>
2021-01-06 00:17:49 +0900

    fix typo for pi0 candidate

--- sangbaek/pi0/pi0.groovy
+++ sangbaek/pi0/pi0.groovy
@@ -33,7 +33,7 @@ class pi0{
           if (status1>=2000 && status2>=2000 && gam1.e() > 2 && gam2.e() > 2){
             def pi0_candidate = gam1 + gam2
             def pi0_mass = pi0_candidate.mass()
-            if (pi0_mass>0.08 && pi0_mass<0.2 && pi0.e() > 2){
+            if (pi0_mass>0.08 && pi0_mass<0.2 && pi0_candidate.e() > 2){
               hists.computeIfAbsent("pi0_mass",h_inv_mass_gg).fill(pi0_mass)
             }
           }
