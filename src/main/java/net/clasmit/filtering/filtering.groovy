ab39d27bc811ebc2d09275758f937660004a2dcc
Sangbaek <sangbaek@mit.edu>
2021-02-07 21:00:05 -0500

    update to version 1.1 include sectors at FILTER::Index add arguments for start and end

--- src/main/java/net/clasmit/filtering/filtering.groovy
+++ src/main/java/net/clasmit/filtering/filtering.groovy
@@ -10,7 +10,6 @@ import net.clasmit.event.EventConverter
 import net.clasmit.pid.electron.ElectronSelector
 import net.clasmit.pid.proton.ProtonSelector
 import net.clasmit.pid.gamma.GammaSelector
-import java.util.concurrent.ConcurrentHashMap
 import org.jlab.clas.pdg.PDGDatabase
 
 class filtering{
@@ -29,6 +28,19 @@ class filtering{
     polarity = polar
     electron_selector = new ElectronSelector(polarity)
     proton_selector = new ProtonSelector(polarity)
+  }
+
+  def determineSector = {hit->
+    def phi = Math.toDegrees(Math.atan2(hit.get(1) /Math.sqrt(hit.get(0)*hit.get(0) + hit.get(1)*hit.get(1) + hit.get(2)*hit.get(2)),
+                  hit.get(0) / Math.sqrt(hit.get(0)*hit.get(0) + hit.get(1)*hit.get(1) + hit.get(2)*hit.get(2))))
+
+    if(phi < 30 && phi >= -30){        return 1;}
+    else if(phi < 90 && phi >= 30){    return 2;}
+    else if(phi < 150 && phi >= 90){   return 3;}
+    else if(phi >= 150 || phi < -150){ return 4;}
+    else if(phi < -90 && phi >= -150){ return 5;}
+    else if(phi < -30 && phi >= -90){  return 6;}
+    return 0
   }
 
   def filterDVCSEvents(event){
@@ -60,10 +72,110 @@ class filtering{
       def electron_candidates = electron_selector.applyCuts(event)
       def proton_candidates = proton_selector.applyCuts(event)
       def gamma_candidates = gamma_selector.applyCuts(event)
-      if (electron_candidates&&proton_candidates&&gamma_candidates) return [*electron_candidates, *proton_candidates, *gamma_candidates]
-      else return null
+      if (electron_candidates&&proton_candidates&&gamma_candidates){
+
+        def pinds = [*electron_candidates, *proton_candidates, *gamma_candidates]
+
+        def pcal_sectors = pinds.collect{index->
+          if (Math.abs(event.status[index])<2000 || Math.abs(event.status[index])>4000) event.status[index]
+          else if(event.pcal_status.contains(index)) event.pcal_sector[index]
+          else 0
+        }
+
+        def ecinner_sectors = pinds.collect{index->
+          if (Math.abs(event.status[index])<2000 || Math.abs(event.status[index])>4000) event.status[index]
+          else if(event.ecal_inner_status.contains(index)) event.ecal_inner_sector[index]
+          else 0
+        }
+
+        def ecouter_sectors = pinds.collect{index->
+          if (Math.abs(event.status[index])<2000 || Math.abs(event.status[index])>4000) event.status[index]
+          else if(event.ecal_outer_status.contains(index)) event.ecal_outer_sector[index]
+          else 0
+        }
+
+        def dc_track_sectors = pinds.collect{index->
+          if (Math.abs(event.status[index])<2000 || Math.abs(event.status[index])>4000) event.status[index]
+          else if(event.dc_sector.containsKey(index)) event.dc_sector[index]
+          else 0
+        }
+
+        def dc1_traj_sectors = pinds.collect{index->
+          if (Math.abs(event.status[index])<2000 || Math.abs(event.status[index])>4000) event.status[index]
+          else if (event.dc1_status.contains(index)){
+            def hit = event.dc1.get(index).find{ hit -> hit.layer == 6}
+            determineSector([hit.x, hit.y, hit.z])
+          }
+          else 0
+        }
+        def dc2_traj_sectors = pinds.collect{index->
+          if (Math.abs(event.status[index])<2000 || Math.abs(event.status[index])>4000) event.status[index]
+          else if (event.dc2_status.contains(index)){
+            def hit = event.dc2.get(index).find{ hit -> hit.layer == 18}
+            determineSector([hit.x, hit.y, hit.z])
+          }
+          else 0
+        }
+
+        def dc3_traj_sectors = pinds.collect{index->
+          if (Math.abs(event.status[index])<2000 || Math.abs(event.status[index])>4000) event.status[index]
+          else if (event.dc3_status.contains(index)){
+            def hit = event.dc3.get(index).find{ hit -> hit.layer == 36}
+            determineSector([hit.x, hit.y, hit.z])
+          }
+          else 0
+        }
+
+        def ftof1a_sectors = pinds.collect{index->
+          if (Math.abs(event.status[index])<2000 || Math.abs(event.status[index])>4000) event.status[index]
+          else if (event.tof_status.contains(index)){
+            def hit = event.tof.get(index).find{ hit -> hit.layer == 1}
+            if (hit) hit.sector
+            else 0
+          }
+          else 0
+        }
+
+        def ftof1b_sectors = pinds.collect{index->
+          if (Math.abs(event.status[index])<2000 || Math.abs(event.status[index])>4000) event.status[index]
+          else if (event.tof_status.contains(index)){
+            def hit = event.tof.get(index).find{ hit -> hit.layer == 2}
+            if (hit) hit.sector
+            else 0
+          }
+          else 0
+        }
+
+        def ftof2_sectors = pinds.collect{index->
+          if (Math.abs(event.status[index])<2000 || Math.abs(event.status[index])>4000) event.status[index]
+          else if (event.tof_status.contains(index)){
+            def hit = event.tof.get(index).find{ hit -> hit.layer == 3}
+            if (hit) hit.sector
+            else 0
+          }
+          else 0
+        }
+
+        def htcc_sectors = pinds.collect{index->
+          if (Math.abs(event.status[index])<2000 || Math.abs(event.status[index])>4000) event.status[index]
+          else if(event.cherenkov_status.contains(index)) event.cherenkov_sector[index]
+          else 0
+        }
+
+        return [pinds:pinds, pcal_sectors:pcal_sectors, ecinner_sectors: ecinner_sectors,
+        ecouter_sectors: ecouter_sectors, dc_track_sectors:dc_track_sectors,
+        dc1_traj_sectors: dc1_traj_sectors, dc2_traj_sectors: dc2_traj_sectors, dc3_traj_sectors: dc3_traj_sectors,
+        ftof1a_sectors: ftof1a_sectors, ftof1b_sectors: ftof1b_sectors, ftof2_sectors: ftof2_sectors, htcc_sectors: htcc_sectors]
+      }
+      else return  [pinds: null, pcal_sectors: null, ecinner_sectors: null,
+      ecouter_sectors: null, dc_track_sectors: null,
+      dc1_traj_sectors: null, dc2_traj_sectors: null, dc3_traj_sectors: null,
+      ftof1a_sectors: null, ftof1b_sectors: null, ftof2_sectors:null, htcc_sectors: null]
     }
-    return null
+    return  [pinds: null, pcal_sectors: null, ecinner_sectors: null,
+    ecouter_sectors: null, dc_track_sectors: null,
+    dc1_traj_sectors: null, dc2_traj_sectors: null, dc3_traj_sectors: null,
+    ftof1a_sectors: null, ftof1b_sectors: null, ftof2_sectors:null, htcc_sectors: null]
   }
 
   def filterGammas(event){
--- src/main/java/net/clasmit/tof.groovy
+++ src/main/java/net/clasmit/tof.groovy
@@ -1,6 +1,5 @@
 package net.clasmit;
 
-// import org.jlab.jnp.hipo4.data.Event;
 import org.jlab.jnp.hipo4.data.Bank;
 import org.jlab.jnp.hipo4.io.HipoReader
 import org.jlab.jnp.hipo4.io.HipoWriter
@@ -14,30 +13,59 @@ import org.jlab.groot.data.H1F
 import org.jlab.groot.data.H2F
 import org.jlab.groot.data.TDirectory
 import groovyx.gpars.GParsPool
-import java.util.concurrent.ConcurrentHashMap
-import java.util.concurrent.Executors
-import java.util.concurrent.ScheduledExecutorService
-import java.util.concurrent.TimeUnit
-import java.util.concurrent.atomic.AtomicInteger
 import groovy.cli.picocli.CliBuilder
 import net.clasmit.filtering.filtering
 import net.clasmit.event.Event
 import net.clasmit.event.EventConverter
 
-def cli = new CliBuilder(usage: 'java -jar target/filter-1.1.jar -[p] polarity file_names')
+
+def cli = new CliBuilder(usage: 'java -jar target/filter-1.1.jar net.clasmit.tof [-p] polarity [-s] start [-e] end file_names')
 
 cli.with {
     h longOpt: 'help', 'Show usage information'
     p longOpt: 'polarity',        defaultValue:'inbending', args: 1, argName: 'polarity',        'inbending| outbending, default is inbending'
+    s longOpt: 'start', defaultValue: "0", args: 1, argName: 'starting event', 'starting event count, or percentage, ex) 1000, or 10%'
+    e longOpt: 'end', defaultValue: "-1", args: 1, argName: 'last event', 'last event count, or percentage, ex) 2000, or 20%'
 }
 
 def options = cli.parse(args)
+
 if (options.h) {
   cli.usage()
   return
 }
+
 def fnames = options.arguments()
 def polarity = options.p
+
+
+def start, end
+def start_mode, end_mode
+
+if (options.s=="0"){
+  start = "0"
+  start_mode = "default"
+}
+
+if (options.e=="-1"){
+  end = "-1"
+  end_mode = "default"
+}
+
+if(!start_mode){
+  start = options.s
+  start_mode = "count"
+  if(start.contains("%")) start_mode = "percentage"
+}  
+
+if(!end_mode){
+  end = options.e
+  end_mode = "count"
+  if(end.contains("%")) end_mode = "percentage"
+}
+
+if (start_mode == "count") start = start as Integer
+if (end_mode == "count") end = end as Integer
 
 println("filtering with "+polarity+" polarity option.")
 
@@ -48,7 +76,18 @@ def jsonbank = '''
         "item" : 99,
         "info": "test",
         "entries": [
-            {"name":"before",      "type":"I", "info":"indices before filtering"}
+            {"name":"before",      "type":"I", "info":"indices before filtering"},
+            {"name":"pcal_sector", "type":"I", "info":"pcal sector"},
+            {"name":"ecinner_sector", "type":"I", "info":"ecal inner sector"},
+            {"name":"ecouter_sector", "type":"I", "info":"ecal outer sector"},
+            {"name":"dc_track_sector", "type":"I", "info":"dc track sector"},
+            {"name":"dc1_traj_sector", "type":"I", "info":"dc1 traj sector"},
+            {"name":"dc2_traj_sector", "type":"I", "info":"dc2 traj sector"},
+            {"name":"dc3_traj_sector", "type":"I", "info":"dc3 traj sector"},
+            {"name":"ftof1a_sector", "type":"I", "info":"ftof1a sector"},
+            {"name":"ftof1b_sector", "type":"I", "info":"ftof1b sector"},
+            {"name":"ftof2_sector", "type":"I", "info":"ftof2 sector"},
+            {"name":"htcc_sector", "type":"I", "info":"htcc sector"}
         ]
     }
 '''
@@ -65,91 +104,124 @@ println("Filtering $outname..")
 outname = outname.substring(0,outname.lastIndexOf(".")) + "_filtered_with_TOF.hipo"
 println("Saving filtered file as $outname...\n\n\n")
 
-def processor = new filtering()
+def processor = new filtering(polarity)
 
-def evcount = new AtomicInteger()
-def debug = {
-  println "event count: "+evcount.get()
-//  evcount.set(0)
+def evcount = 0
+def saved = 0
+def debug = {evc->
+  println("processing "+evc+"-th event")
 }
 
-def exe = Executors.newScheduledThreadPool(1)
-exe.scheduleWithFixedDelay(debug, 5, 30, TimeUnit.SECONDS)
+fnames.each{fname->
 
-GParsPool.withPool 12, {
-  fnames.eachParallel{fname->
+  def reader = new HipoReader()
+  reader.open(fname)
+  
+  // set up the range of filtering
+  def totalEvent = reader.getEventCount()
+  if (start == "0" || start == "0%") start = 0
+  else if (start_mode =="percentage") start = Math.round(Float.valueOf(start[0..-2])*totalEvent/100)
+  if (end == "-1" || end == "100%") end = totalEvent
+  else if (end_mode =="percentage") end = Math.round(Float.valueOf(end[0..-2])*totalEvent/100)
+  
+  println("Requested from events "+start+ " to " + end)
 
-    def reader = new HipoReader()
-    reader.open(fname)
-    SchemaFactory schema = reader.getSchemaFactory();
-    // SchemaFactory writerFactory = schema.reduce(["REC::Particle", "RUN::config", "REC::Event"]);
-    // defining schema with REC::Scintillator
-    SchemaFactory writerFactory = schema.reduce(["REC::Particle", "REC::Scintillator", "RUN::config", "REC::Event"]);
-    writerFactory.addSchema(customSchema)
-    def writer = new HipoWriter(writerFactory)
+  SchemaFactory schema = reader.getSchemaFactory();
+  // SchemaFactory writerFactory = schema.reduce(["REC::Particle", "RUN::config", "REC::Event"]);
+  // defining schema with REC::Scintillator
+  SchemaFactory writerFactory = schema.reduce(["REC::Particle", "REC::Scintillator", "RUN::config", "REC::Event"]);
+  writerFactory.addSchema(customSchema)
+  def writer = new HipoWriter(writerFactory)
 
-    writer.open(outname)
+  writer.open(outname)
 
-    BankIterator           iterPart = new BankIterator(4096);
-    BankSelector   dataSelectorPart = new BankSelector(schema.getSchema("REC::Particle"));
-    // defining iterator and selector for filtering REC::Scintillator
-    BankIterator           iterScin = new BankIterator(4096);
-    BankSelector   dataSelectorScin = new BankSelector(schema.getSchema("REC::Scintillator"));
+  BankIterator           iterPart = new BankIterator(4096);
+  BankSelector   dataSelectorPart = new BankSelector(schema.getSchema("REC::Particle"));
+  // defining iterator and selector for filtering REC::Scintillator
+  BankIterator           iterScin = new BankIterator(4096);
+  BankSelector   dataSelectorScin = new BankSelector(schema.getSchema("REC::Scintillator"));
 
-    def jnp_event = new org.jlab.jnp.hipo4.data.Event()
+  def jnp_event = new org.jlab.jnp.hipo4.data.Event()
 
-    while(reader.hasNext()) {
-      evcount.getAndIncrement()
-      reader.nextEvent(jnp_event)
-      if(!jnp_event.hasBank(reader.getSchemaFactory().getSchema("REC::Particle"))) continue
-      def data_event = new HipoDataEvent(jnp_event, reader.getSchemaFactory())
-      def event = EventConverter.convert(data_event)
-      def partList = processor.filterEPGs(event)// get columns of REC::Particle to be saved
-      //saves only such columns
-      if (partList) {
-        dataSelectorPart.getIterator(jnp_event, iterPart);
-        iterPart.reset()
+  while(evcount<totalEvent) {
 
-        // preparing for REC::Scintillator filtering
-        dataSelectorScin.getIterator(jnp_event, iterScin);
-        iterScin.reset()
-        
-        // preparing for FILTER::Index
-        def customBank = new Bank(customSchema, partList.size)
-
-        //filtering REC::Particle
-        partList.eachWithIndex{val, index ->
-          iterPart.addIndex(val)
-          customBank.putInt("before", index, val) //saving old pindex at FILTER::Index:before
-        }
-
-        //filtering REC::Scintillator
-        Bank recScinBank = dataSelectorScin.getBank()
-        if (recScinBank.getRows()>0){
-          (0..<recScinBank.getRows()).each{
-            def pindex = recScinBank.getInt("pindex", it)
-            if (partList.contains(pindex)) iterScin.addIndex(it)
-          }
-        }
-
-        //saving filtered REC::Particle
-        Bank newPartBank = BankSelector.reduceBank(dataSelectorPart.getBank(), iterPart);
-        jnp_event.remove(dataSelectorPart.getBank().getSchema());
-        jnp_event.write(newPartBank); // REC::Particle only passed filtering.filterEPGs
-
-        jnp_event.write(customBank)//FILTER::Index bank
-
-        //saving filtered REC::Scintillator
-        Bank newScinScin = BankSelector.reduceBank(recScinBank, iterScin);
-        jnp_event.remove(dataSelectorScin.getBank().getSchema());
-        jnp_event.write(newScinScin);
-        writer.addEvent(jnp_event)
-      }
+    if (evcount<start){
+      evcount++
+      continue
     }
-    writer.close()
-    reader.close()
-  }
-}
+    else if (evcount==start) debug(evcount)
+    else if (evcount==end) {
+      debug(evcount-1)
+      break
+    }
+    else if (evcount%100000==0) debug(evcount)
+    
+    evcount++
+    reader.nextEvent(jnp_event)
 
-exe.shutdown()
-debug()
+    if(!jnp_event.hasBank(reader.getSchemaFactory().getSchema("REC::Particle"))) continue
+    def data_event = new HipoDataEvent(jnp_event, reader.getSchemaFactory())
+    def event = EventConverter.convert(data_event)
+    def partDict = processor.filterEPGs(event)// get columns of REC::Particle to be saved
+    def partList = partDict["pinds"]// get columns of REC::Particle to be saved
+
+    //saves only such columns
+    if (partList) {
+
+      saved ++
+
+      dataSelectorPart.getIterator(jnp_event, iterPart);
+      iterPart.reset()
+
+      // preparing for REC::Scintillator
+      dataSelectorScin.getIterator(jnp_event, iterScin);
+      iterScin.reset()
+      
+      // preparing for FILTER::Index
+      def customBank = new Bank(customSchema, partList.size)
+
+      //filtering REC::Particle
+      partList.eachWithIndex{val, index ->
+        iterPart.addIndex(val)
+        customBank.putInt("before", index, val) //saving old pindex at FILTER::Index:before
+        customBank.putInt("pcal_sector", index, partDict.pcal_sectors[index]) //saving pcal sectors at FILTER::Index:before
+        customBank.putInt("ecinner_sector", index, partDict.ecinner_sectors[index]) //saving ec inner sectors at FILTER::Index:before
+        customBank.putInt("ecouter_sector", index, partDict.ecouter_sectors[index]) //saving ec outer sectors at FILTER::Index:before
+        customBank.putInt("dc_track_sector", index, partDict.dc_track_sectors[index]) //saving dc track sectors at FILTER::Index:before
+        customBank.putInt("dc1_traj_sector", index, partDict.dc1_traj_sectors[index]) //saving dc1 traj sectors at FILTER::Index:before
+        customBank.putInt("dc2_traj_sector", index, partDict.dc2_traj_sectors[index]) //saving dc2 traj sectors at FILTER::Index:before
+        customBank.putInt("dc3_traj_sector", index, partDict.dc3_traj_sectors[index]) //saving dc3 traj sectors at FILTER::Index:before
+        customBank.putInt("ftof1a_sector", index, partDict.ftof1a_sectors[index]) //saving ftof 1a sectors at FILTER::Index:before
+        customBank.putInt("ftof1b_sector", index, partDict.ftof1b_sectors[index]) //saving ftof 1b sectors at FILTER::Index:before
+        customBank.putInt("ftof2_sector", index, partDict.ftof2_sectors[index]) //saving ftof 2 sectors at FILTER::Index:before
+        customBank.putInt("htcc_sector", index, partDict.htcc_sectors[index]) //saving cherenkov sectors at FILTER::Index:before
+      }
+
+      //filtering REC::Scintillator
+      Bank recScinBank = dataSelectorScin.getBank()
+      if (recScinBank.getRows()>0){
+        (0..<recScinBank.getRows()).each{
+          def pindex = recScinBank.getInt("pindex", it)
+          if (partList.contains(pindex)) iterScin.addIndex(it)
+        }
+      }
+
+      //saving filtered REC::Particle
+      Bank newPartBank = BankSelector.reduceBank(dataSelectorPart.getBank(), iterPart);
+      jnp_event.remove(dataSelectorPart.getBank().getSchema());
+      jnp_event.write(newPartBank); // REC::Particle only passed filtering.filterEPGs
+
+      jnp_event.write(customBank)//saving FILTER::Index bank
+
+      //saving filtered REC::Scintillator
+      Bank newScinScin = BankSelector.reduceBank(recScinBank, iterScin);
+      jnp_event.remove(dataSelectorScin.getBank().getSchema());
+      jnp_event.write(newScinScin);
+      writer.addEvent(jnp_event)
+    }
+  }
+  print("Saved "+saved +" events out of ")
+  println(end-start + " requested events")
+  writer.close()
+  reader.close()
+}
